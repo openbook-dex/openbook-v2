f18b0665fa9dfd77c9f005539d7e6c223e157447
Binye Barwe <binyebarwe@gmail.com>
2023-04-26 11:22:43 +0200

    fmt

--- programs/openbook-v2/src/instructions/deposit.rs
+++ programs/openbook-v2/src/instructions/deposit.rs
@@ -1,8 +1,8 @@
 use crate::accounts_ix::Deposit;
+use crate::logs::DepositLog;
 use crate::state::*;
 use anchor_lang::prelude::*;
 use anchor_spl::token::{self, Transfer};
-use crate::logs::DepositLog;
 
 pub fn deposit(ctx: Context<Deposit>, base_amount_lots: u64, quote_amount_lots: u64) -> Result<()> {
     let mut open_orders_account = ctx.accounts.open_orders_account.load_full_mut()?;
@@ -23,11 +23,11 @@ pub fn deposit(ctx: Context<Deposit>, base_amount_lots: u64, quote_amount_lots: 
         )?;
         open_orders_account.fixed.position.base_free_lots += base_amount_lots as i64;
 
-        emit!(DepositLog{
+        emit!(DepositLog {
             open_orders_acc: ctx.accounts.open_orders_account.key(),
-           signer: ctx.accounts.owner.key(),
-           quantity: base_amount_lots * (market.base_lot_size as u64),
-       });
+            signer: ctx.accounts.owner.key(),
+            quantity: base_amount_lots * (market.base_lot_size as u64),
+        });
     }
 
     if quote_amount_lots != 0 {
@@ -46,8 +46,8 @@ pub fn deposit(ctx: Context<Deposit>, base_amount_lots: u64, quote_amount_lots: 
 
         open_orders_account.fixed.position.quote_free_lots += quote_amount_lots as i64;
 
-        emit!(DepositLog{
-             open_orders_acc: ctx.accounts.open_orders_account.key(),
+        emit!(DepositLog {
+            open_orders_acc: ctx.accounts.open_orders_account.key(),
             signer: ctx.accounts.owner.key(),
             quantity: quote_amount_lots * (market.quote_lot_size as u64),
         });
