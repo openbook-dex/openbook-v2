7ae214e8af4f95fbe94d1f0534b74d4bfb2a81e0
skrrb <skrrb@pm.me>
2023-05-10 16:08:14 +0200

    anchor idl helper types (missing file) [skip ci]

---
+++ programs/openbook-v2/src/types.rs
@@ -0,0 +1,14 @@
+use anchor_lang::prelude::*;
+
+/// Nothing in Rust shall use these types. They only exist so that the Anchor IDL
+/// knows about them and typescript can deserialize it.
+
+#[derive(AnchorSerialize, AnchorDeserialize, Default)]
+pub struct MarketIndex {
+    val: u32,
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Default)]
+pub struct I80F48 {
+    val: i128,
+}
