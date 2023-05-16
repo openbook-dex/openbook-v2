f90059603e515a68f880bffc3b9d18375f451f81
skrrb <skrrb@pm.me>
2023-05-16 11:41:21 +0200

    wip

--- programs/openbook-v2/tests/cases/test_self_trade.rs
+++ programs/openbook-v2/tests/cases/test_self_trade.rs
@@ -7,22 +7,24 @@ async fn test_self_trade_decrement_take() -> Result<(), TransportError> {
     let solana = &context.solana.clone();
 
     let admin = TestKeypair::new();
-    let owner = context.users[0].key;
-    let payer = context.users[1].key;
-    let mints = &context.mints[0..=2];
+    let payer = &context.users[0];
+    let owner = context.users[1].key;
+    let owner_base_ata = context.users[1].token_accounts[0];
+    let owner_quote_ata = context.users[1].token_accounts[1];
 
-    let owner_token_0 = context.users[0].token_accounts[0];
-    let owner_token_1 = context.users[0].token_accounts[1];
+    let mints = &context.mints[0..2];
+    let tokens = Token::create(mints.to_vec(), solana, admin, payer.key).await;
 
-    let tokens = Token::create(mints.to_vec(), solana, admin, payer).await;
+    let base_mint = context.mints[0].pubkey;
+    let quote_mint = context.mints[1].pubkey;
 
     // TEST: Create a market
     let market = get_market_address(admin.pubkey(), 1);
     let base_vault = solana
-        .create_associated_token_account(&market, mints[0].pubkey)
+        .create_associated_token_account(&market, base_mint)
         .await;
     let quote_vault = solana
-        .create_associated_token_account(&market, mints[1].pubkey)
+        .create_associated_token_account(&market, quote_mint)
         .await;
 
     let openbook_v2::accounts::CreateMarket {
@@ -34,14 +36,14 @@ async fn test_self_trade_decrement_take() -> Result<(), TransportError> {
         solana,
         CreateMarketInstruction {
             admin,
-            payer,
+            payer: payer.key,
             market_index: 1,
             quote_lot_size: 10,
             base_lot_size: 100,
             maker_fee: -0.0002,
             taker_fee: 0.0004,
-            base_mint: mints[0].pubkey,
-            quote_mint: mints[1].pubkey,
+            base_mint,
+            quote_mint,
             base_vault,
             quote_vault,
             ..CreateMarketInstruction::with_new_book_and_queue(solana, &tokens[1]).await
@@ -50,8 +52,8 @@ async fn test_self_trade_decrement_take() -> Result<(), TransportError> {
     .await
     .unwrap();
 
-    let account_0 = create_funded_account(solana, owner, market, 0, &context.users[1]).await;
-    let account_1 = create_funded_account(solana, owner, market, 1, &context.users[1]).await;
+    let account_0 = create_open_orders_account(solana, owner, market, 0, &payer).await;
+    let account_1 = create_open_orders_account(solana, owner, market, 1, &payer).await;
 
     println!("\n\nTAKER LIMIT ORDER\n\n");
 
@@ -62,38 +64,13 @@ async fn test_self_trade_decrement_take() -> Result<(), TransportError> {
             open_orders_account: account_0,
             market,
             owner,
-            payer: owner_token_1,
-            base_vault,
-            quote_vault,
-            side: Side::Bid,
-            price_lots: 1000,
-            max_base_lots: 2,
-            max_quote_lots_including_fees: 10000,
-            client_order_id: 0,
-            expiry_timestamp: 0,
-            order_type: PlaceOrderType::Limit,
-            self_trade_behavior: SelfTradeBehavior::default(),
-        },
-    )
-    .await
-    .unwrap();
-
-    println!("\n\nMAKER LIMIT ORDER\n\n");
-    // maker limit order
-    send_tx(
-        solana,
-        PlaceOrderInstruction {
-            open_orders_account: account_1,
-            market,
-            owner,
-            payer: owner_token_0,
+            payer: owner_base_ata,
             base_vault,
             quote_vault,
             side: Side::Ask,
             price_lots: 1000,
             max_base_lots: 2,
             max_quote_lots_including_fees: 10000,
-
             client_order_id: 1,
             expiry_timestamp: 0,
             order_type: PlaceOrderType::Limit,
@@ -103,7 +80,29 @@ async fn test_self_trade_decrement_take() -> Result<(), TransportError> {
     .await
     .unwrap();
 
-    println!("\n\nTAKER SELF_TRADE\n\n");
+    // maker limit order
+    send_tx(
+        solana,
+        PlaceOrderInstruction {
+            open_orders_account: account_1,
+            market,
+            owner,
+            payer: owner_base_ata,
+            base_vault,
+            quote_vault,
+            side: Side::Ask,
+            price_lots: 1000,
+            max_base_lots: 2,
+            max_quote_lots_including_fees: 10000,
+            client_order_id: 2,
+            expiry_timestamp: 0,
+            order_type: PlaceOrderType::Limit,
+            self_trade_behavior: SelfTradeBehavior::default(),
+        },
+    )
+    .await
+    .unwrap();
+
     // taker full self-trade IOC
     send_tx(
         solana,
@@ -111,14 +110,14 @@ async fn test_self_trade_decrement_take() -> Result<(), TransportError> {
             open_orders_account: account_0,
             market,
             owner,
-            payer: owner_token_1,
+            payer: owner_quote_ata,
             base_vault,
             quote_vault,
             side: Side::Bid,
             price_lots: 1000,
             max_base_lots: 1,
             max_quote_lots_including_fees: 10000,
-            client_order_id: 2,
+            client_order_id: 3,
             expiry_timestamp: 0,
             order_type: PlaceOrderType::ImmediateOrCancel,
             self_trade_behavior: SelfTradeBehavior::DecrementTake,
@@ -127,115 +126,137 @@ async fn test_self_trade_decrement_take() -> Result<(), TransportError> {
     .await
     .unwrap();
 
+    // taker partial self-trade limit
+    send_tx(
+        solana,
+        PlaceOrderInstruction {
+            open_orders_account: account_0,
+            market,
+            owner,
+            payer: owner_quote_ata,
+            base_vault,
+            quote_vault,
+            side: Side::Bid,
+            price_lots: 1000,
+            max_base_lots: 2,
+            max_quote_lots_including_fees: 10000,
+            client_order_id: 4,
+            expiry_timestamp: 0,
+            order_type: PlaceOrderType::Limit,
+            self_trade_behavior: SelfTradeBehavior::DecrementTake,
+        },
+    )
+    .await
+    .unwrap();
 
 
-    //{
-    //    let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
-    //    let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
+    {
+        //let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
+        //let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
 
-    //    assert_eq!(open_orders_account_0.position.bids_base_lots, 1);
-    //    assert_eq!(open_orders_account_1.position.bids_base_lots, 0);
-    //    assert_eq!(open_orders_account_0.position.asks_base_lots, 0);
-    //    assert_eq!(open_orders_account_1.position.asks_base_lots, 0);
-    //    assert_eq!(open_orders_account_0.position.base_free_native, 0);
-    //    assert_eq!(open_orders_account_1.position.base_free_native, 0);
-    //    assert_eq!(open_orders_account_0.position.quote_free_native, 0);
-    //    assert_eq!(
-    //        open_orders_account_1.position.quote_free_native.round(),
-    //        99960
-    //    );
-    //}
+        //assert_eq!(open_orders_account_0.position.bids_base_lots, 1);
+        //assert_eq!(open_orders_account_1.position.bids_base_lots, 0);
+        //assert_eq!(open_orders_account_0.position.asks_base_lots, 0);
+        //assert_eq!(open_orders_account_1.position.asks_base_lots, 0);
+        //assert_eq!(open_orders_account_0.position.base_free_native, 0);
+        //assert_eq!(open_orders_account_1.position.base_free_native, 0);
+        //assert_eq!(open_orders_account_0.position.quote_free_native, 0);
+        //assert_eq!(
+        //    open_orders_account_1.position.quote_free_native.round(),
+        //    99960
+        //);
+    }
 
-    //send_tx(
-    //    solana,
-    //    ConsumeEventsInstruction {
-    //        market,
-    //        open_orders_accounts: vec![account_0, account_1],
-    //    },
-    //)
-    //.await
-    //.unwrap();
+    ////send_tx(
+    ////    solana,
+    ////    ConsumeEventsInstruction {
+    ////        market,
+    ////        open_orders_accounts: vec![account_0, account_1],
+    ////    },
+    ////)
+    ////.await
+    ////.unwrap();
 
-    //{
-    //    let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
-    //    let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
+    ////{
+    ////    let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
+    ////    let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
 
-    //    assert_eq!(open_orders_account_0.position.bids_base_lots, 0);
-    //    assert_eq!(open_orders_account_1.position.bids_base_lots, 0);
-    //    assert_eq!(open_orders_account_0.position.asks_base_lots, 0);
-    //    assert_eq!(open_orders_account_1.position.asks_base_lots, 0);
-    //    assert_eq!(open_orders_account_0.position.base_free_native, 100);
-    //    assert_eq!(open_orders_account_1.position.base_free_native, 0);
-    //    assert_eq!(open_orders_account_0.position.quote_free_native.round(), 20);
-    //    assert_eq!(
-    //        open_orders_account_1.position.quote_free_native.round(),
-    //        99960
-    //    );
-    //}
+    ////    assert_eq!(open_orders_account_0.position.bids_base_lots, 0);
+    ////    assert_eq!(open_orders_account_1.position.bids_base_lots, 0);
+    ////    assert_eq!(open_orders_account_0.position.asks_base_lots, 0);
+    ////    assert_eq!(open_orders_account_1.position.asks_base_lots, 0);
+    ////    assert_eq!(open_orders_account_0.position.base_free_native, 100);
+    ////    assert_eq!(open_orders_account_1.position.base_free_native, 0);
+    ////    assert_eq!(open_orders_account_0.position.quote_free_native.round(), 20);
+    ////    assert_eq!(
+    ////        open_orders_account_1.position.quote_free_native.round(),
+    ////        99960
+    ////    );
+    ////}
 
-    //send_tx(
-    //    solana,
-    //    SettleFundsInstruction {
-    //        market,
-    //        open_orders_account: account_0,
-    //        base_vault,
-    //        quote_vault,
-    //        payer_base: owner_token_0,
-    //        payer_quote: owner_token_1,
-    //        referrer: None,
-    //    },
-    //)
-    //.await
-    //.unwrap();
+    ////send_tx(
+    ////    solana,
+    ////    SettleFundsInstruction {
+    ////        market,
+    ////        open_orders_account: account_0,
+    ////        base_vault,
+    ////        quote_vault,
+    ////        payer_base: owner_token_0,
+    ////        payer_quote: owner_token_1,
+    ////        referrer: None,
+    ////    },
+    ////)
+    ////.await
+    ////.unwrap();
 
-    //{
-    //    let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
-    //    let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
+    ////{
+    ////    let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
+    ////    let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
 
-    //    assert_eq!(open_orders_account_0.position.base_free_native, 0);
-    //    assert_eq!(open_orders_account_1.position.base_free_native, 0);
-    //    assert_eq!(open_orders_account_0.position.quote_free_native, 0);
-    //    assert_eq!(
-    //        open_orders_account_1.position.quote_free_native.round(),
-    //        99960
-    //    );
-    //}
+    ////    assert_eq!(open_orders_account_0.position.base_free_native, 0);
+    ////    assert_eq!(open_orders_account_1.position.base_free_native, 0);
+    ////    assert_eq!(open_orders_account_0.position.quote_free_native, 0);
+    ////    assert_eq!(
+    ////        open_orders_account_1.position.quote_free_native.round(),
+    ////        99960
+    ////    );
+    ////}
 
-    //send_tx(
-    //    solana,
-    //    SettleFundsInstruction {
-    //        market,
-    //        open_orders_account: account_1,
-    //        base_vault,
-    //        quote_vault,
-    //        payer_base: owner_token_0,
-    //        payer_quote: owner_token_1,
-    //        referrer: None,
-    //    },
-    //)
-    //.await
-    //.unwrap();
+    ////send_tx(
+    ////    solana,
+    ////    SettleFundsInstruction {
+    ////        market,
+    ////        open_orders_account: account_1,
+    ////        base_vault,
+    ////        quote_vault,
+    ////        payer_base: owner_token_0,
+    ////        payer_quote: owner_token_1,
+    ////        referrer: None,
+    ////    },
+    ////)
+    ////.await
+    ////.unwrap();
 
-    //{
-    //    let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
-    //    let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
+    ////{
+    ////    let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
+    ////    let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
 
-    //    assert_eq!(open_orders_account_0.position.base_free_native, 0);
-    //    assert_eq!(open_orders_account_1.position.base_free_native, 0);
-    //    assert_eq!(open_orders_account_0.position.quote_free_native, 0);
-    //    assert_eq!(open_orders_account_1.position.quote_free_native, 0);
-    //}
+    ////    assert_eq!(open_orders_account_0.position.base_free_native, 0);
+    ////    assert_eq!(open_orders_account_1.position.base_free_native, 0);
+    ////    assert_eq!(open_orders_account_0.position.quote_free_native, 0);
+    ////    assert_eq!(open_orders_account_1.position.quote_free_native, 0);
+    ////}
 
-    //send_tx(
-    //    solana,
-    //    CloseMarketInstruction {
-    //        admin,
-    //        market,
-    //        sol_destination: owner.pubkey(),
-    //    },
-    //)
-    //.await
-    //.unwrap();
+    ////send_tx(
+    ////    solana,
+    ////    CloseMarketInstruction {
+    ////        admin,
+    ////        market,
+    ////        sol_destination: owner.pubkey(),
+    ////    },
+    ////)
+    ////.await
+    ////.unwrap();
 
     Ok(())
 }
