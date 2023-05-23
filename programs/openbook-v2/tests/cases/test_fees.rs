7741402c10b0ef4a9915ec86d3640d9f0f4cd25f
Binye Barwe <binyebarwe@gmail.com>
2023-05-23 09:42:57 +0200

    round errors after u64 -> i64

--- programs/openbook-v2/tests/cases/test_fees.rs
+++ programs/openbook-v2/tests/cases/test_fees.rs
@@ -194,7 +194,7 @@ async fn test_fees_accrued() -> Result<(), TransportError> {
     {
         let market = solana.get_account::<Market>(market).await;
         assert_eq!(market.quote_fees_accrued, 0);
-        assert_eq!(market.fees_accrued, 10);
+        assert_eq!(market.fees_accrued, 9);
         assert_eq!(market.fees_to_referrers, 0);
     }
 
@@ -227,7 +227,7 @@ async fn test_fees_accrued() -> Result<(), TransportError> {
     {
         let market = solana.get_account::<Market>(market).await;
         assert_eq!(market.quote_fees_accrued, 1000);
-        assert_eq!(market.fees_accrued, 10);
+        assert_eq!(market.fees_accrued, 9);
         assert_eq!(market.fees_to_referrers, 0);
         assert_eq!(
             balance_quote - fee_penalty,
