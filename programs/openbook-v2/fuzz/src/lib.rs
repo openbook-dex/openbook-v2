6fa2537f7c163e917fc35151db4e56d49745f3bb
skrrb <skrrb@pm.me>
2023-06-02 11:06:58 +0200

    account allocator

--- programs/openbook-v2/fuzz/src/lib.rs
+++ programs/openbook-v2/fuzz/src/lib.rs
@@ -1,7 +1,12 @@
+pub mod account_allocator;
+use account_allocator::*;
+
 pub struct FuzzContext {}
 
 impl FuzzContext {
     pub fn new() -> Self {
+        let _allocator = AccountAllocator::new();
+
         Self {}
     }
 }
