83fed6102971e07710326db5d153150fbde7efd9
skrrb <skrrb@pm.me>
2023-06-03 12:19:46 +0200

    create market

--- programs/openbook-v2/fuzz/src/account_allocator.rs
+++ programs/openbook-v2/fuzz/src/account_allocator.rs
@@ -4,7 +4,7 @@ use solana_program::{
     account_info::AccountInfo, bpf_loader, clock::Epoch, program_pack::Pack, pubkey::Pubkey,
     rent::Rent, system_program,
 };
-use spl_token::state::Mint;
+use spl_token::state::{Account, Mint};
 
 pub struct AccountAllocator(Bump);
 
@@ -45,6 +45,28 @@ impl AccountAllocator {
         )
     }
 
+    pub fn new_ata(&self, owner: &Pubkey, mint: &Pubkey) -> AccountInfo {
+        let bump = &self.0;
+        let data = bump.alloc_slice_fill_copy(Account::LEN, 0u8);
+        let mut account = Account::default();
+        account.state = spl_token::state::AccountState::Initialized;
+        account.mint = *mint;
+        account.owner = *owner;
+        Account::pack(account, data).unwrap();
+
+        let pubkey = spl_associated_token_account::get_associated_token_address(owner, mint);
+        AccountInfo::new(
+            bump.alloc(pubkey),
+            false,
+            true,
+            bump.alloc(Rent::default().minimum_balance(data.len())),
+            data,
+            &spl_token::ID,
+            false,
+            Epoch::default(),
+        )
+    }
+
     pub fn new_program(&self, pubkey: Pubkey) -> AccountInfo {
         let bump = &self.0;
         AccountInfo::new(
@@ -59,14 +81,65 @@ impl AccountAllocator {
         )
     }
 
-    pub fn new_stub_oracle(&self, mint: &Pubkey) -> AccountInfo {
+    pub fn new_market(&self) -> AccountInfo {
+        let market_index: MarketIndex = 0;
+        let (pubkey, _bump) = Pubkey::find_program_address(
+            &[b"Market".as_ref(), market_index.to_le_bytes().as_ref()],
+            &openbook_v2::ID,
+        );
+
+        let len = 8 + std::mem::size_of::<Market>();
         let bump = &self.0;
+        AccountInfo::new(
+            bump.alloc(pubkey),
+            false,
+            true,
+            bump.alloc(Rent::default().minimum_balance(len)),
+            bump.alloc_slice_fill_copy(len, 0u8),
+            &openbook_v2::ID,
+            false,
+            Epoch::default(),
+        )
+    }
+
+    pub fn new_bookside(&self) -> AccountInfo {
+        let bump = &self.0;
+        let len = 8 + std::mem::size_of::<BookSide>();
+        AccountInfo::new(
+            bump.alloc(Pubkey::new_unique()),
+            false,
+            true,
+            bump.alloc(Rent::default().minimum_balance(len)),
+            bump.alloc_slice_fill_copy(len, 0u8),
+            &openbook_v2::ID,
+            false,
+            Epoch::default(),
+        )
+    }
+
+    pub fn new_event_queue(&self) -> AccountInfo {
+        let bump = &self.0;
+        let len = 8 + std::mem::size_of::<EventQueue>();
+        AccountInfo::new(
+            bump.alloc(Pubkey::new_unique()),
+            false,
+            true,
+            bump.alloc(Rent::default().minimum_balance(len)),
+            bump.alloc_slice_fill_copy(len, 0u8),
+            &openbook_v2::ID,
+            false,
+            Epoch::default(),
+        )
+    }
+
+    pub fn new_stub_oracle(&self, mint: &Pubkey) -> AccountInfo {
         let (pubkey, _bump) = Pubkey::find_program_address(
             &[b"StubOracle".as_ref(), mint.as_ref()],
             &openbook_v2::ID,
         );
 
         let len = 8 + std::mem::size_of::<StubOracle>();
+        let bump = &self.0;
         AccountInfo::new(
             bump.alloc(pubkey),
             false,
--- programs/openbook-v2/fuzz/src/lib.rs
+++ programs/openbook-v2/fuzz/src/lib.rs
@@ -3,8 +3,9 @@ pub mod processor;
 
 use account_allocator::*;
 use fixed::types::I80F48;
+use openbook_v2::state::OracleConfigParams;
 use processor::*;
-use solana_program::{instruction::Instruction, system_program};
+use solana_program::{instruction::Instruction, pubkey::Pubkey, system_program};
 
 pub struct FuzzContext {}
 
@@ -14,23 +15,90 @@ impl FuzzContext {
 
         let payer = allocator.new_signer(1_000_000);
         let admin = allocator.new_signer(1_000_000);
-        let mint = allocator.new_mint();
-        let oracle = allocator.new_stub_oracle(mint.key);
+
         let system_program = allocator.new_program(system_program::ID);
+
+        let market = allocator.new_market();
+        let base_mint = allocator.new_mint();
+        let quote_mint = allocator.new_mint();
+
+        let oracle = allocator.new_stub_oracle(base_mint.key);
+
+        let bids = allocator.new_bookside();
+        let asks = allocator.new_bookside();
+        let event_queue = allocator.new_event_queue();
+
+        let base_vault = allocator.new_ata(market.key, base_mint.key);
+        let quote_vault = allocator.new_ata(market.key, quote_mint.key);
 
         let instruction = {
             let accounts = openbook_v2::accounts::StubOracleCreate {
                 oracle: *oracle.key,
                 admin: *admin.key,
-                mint: *mint.key,
+                mint: *base_mint.key,
                 payer: *payer.key,
                 system_program: *system_program.key,
             };
             let data = openbook_v2::instruction::StubOracleCreate { price: I80F48::ONE };
             make_instruction(&accounts, data)
         };
-        let accounts = vec![oracle, admin, mint, payer, system_program];
-        do_process_instruction(instruction, &accounts);
+        let accounts = vec![
+            oracle.clone(),
+            admin.clone(),
+            base_mint.clone(),
+            payer.clone(),
+            system_program.clone(),
+        ];
+        do_process_instruction(instruction, &accounts).unwrap();
+
+        let instruction = {
+            let accounts = openbook_v2::accounts::CreateMarket {
+                market: *market.key,
+                bids: *bids.key,
+                asks: *asks.key,
+                event_queue: *event_queue.key,
+                payer: *payer.key,
+                base_vault: *base_vault.key,
+                quote_vault: *quote_vault.key,
+                base_mint: *base_mint.key,
+                quote_mint: *quote_mint.key,
+                system_program: *system_program.key,
+                oracle: *oracle.key,
+            };
+
+            let data = openbook_v2::instruction::CreateMarket {
+                market_index: 0,
+                name: "fuzz_market".to_string(),
+                oracle_config: OracleConfigParams {
+                    conf_filter: 0.1,
+                    max_staleness_slots: None,
+                },
+                quote_lot_size: 10,
+                base_lot_size: 100,
+                maker_fee: -0.0002,
+                taker_fee: 0.0004,
+                fee_penalty: 0,
+                collect_fee_admin: Pubkey::new_unique(),
+                open_orders_admin: None,
+                consume_events_admin: None,
+                close_market_admin: None,
+            };
+            make_instruction(&accounts, data)
+        };
+        let accounts = vec![
+            market.clone(),
+            bids.clone(),
+            asks.clone(),
+            event_queue.clone(),
+            payer.clone(),
+            base_vault.clone(),
+            quote_vault.clone(),
+            base_mint.clone(),
+            quote_mint.clone(),
+            system_program.clone(),
+            oracle.clone(),
+        ];
+        do_process_instruction(instruction, &accounts).unwrap();
 
         Self {}
     }
