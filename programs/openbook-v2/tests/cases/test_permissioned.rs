c8d5a5c48b9cddfdd4a97401aa9c05eca374fe4f
Binye Barwe <binyebarwe@gmail.com>
2023-06-14 10:34:37 +0200

    add test to prune orders and close market

--- programs/openbook-v2/tests/cases/test_permissioned.rs
+++ programs/openbook-v2/tests/cases/test_permissioned.rs
@@ -264,3 +264,212 @@ async fn test_consume_events_admin() -> Result<(), TransportError> {
 
     Ok(())
 }
+
+#[tokio::test]
+async fn test_close_market_admin() -> Result<(), TransportError> {
+    let TestInitialize {
+        context,
+        close_market_admin,
+        collect_fee_admin,
+        owner,
+        owner_token_0,
+        owner_token_1,
+        market,
+        base_vault,
+        quote_vault,
+        price_lots,
+        tokens,
+        account_0,
+        account_1,
+        ..
+    } = TestContext::new_with_market(TestNewMarketInitialize {
+        close_market_admin_bool: true,
+        ..TestNewMarketInitialize::default()
+    })
+    .await?;
+    let solana = &context.solana.clone();
+
+    // Set the initial oracle price
+    set_stub_oracle_price(solana, &tokens[1], collect_fee_admin, 1000.0).await;
+
+    send_tx(
+        solana,
+        PlaceOrderInstruction {
+            open_orders_account: account_0,
+            open_orders_admin: None,
+            market,
+            owner,
+            token_deposit_account: owner_token_1,
+            base_vault,
+            quote_vault,
+            side: Side::Bid,
+            price_lots,
+            max_base_lots: 1,
+            max_quote_lots_including_fees: 10000,
+
+            client_order_id: 0,
+            expiry_timestamp: 0,
+            order_type: PlaceOrderType::Limit,
+            self_trade_behavior: SelfTradeBehavior::default(),
+            remainings: vec![],
+        },
+    )
+    .await
+    .unwrap();
+
+    // Place an order that matches
+    send_tx(
+        solana,
+        PlaceOrderInstruction {
+            open_orders_account: account_1,
+            open_orders_admin: None,
+            market,
+            owner,
+            token_deposit_account: owner_token_0,
+            base_vault,
+            quote_vault,
+            side: Side::Ask,
+            price_lots,
+            max_base_lots: 1,
+            max_quote_lots_including_fees: 10000,
+
+            client_order_id: 0,
+            expiry_timestamp: 0,
+            order_type: PlaceOrderType::Limit,
+            self_trade_behavior: SelfTradeBehavior::default(),
+            remainings: vec![],
+        },
+    )
+    .await
+    .unwrap();
+
+    // Place another order
+    send_tx(
+        solana,
+        PlaceOrderInstruction {
+            open_orders_account: account_0,
+            open_orders_admin: None,
+            market,
+            owner,
+            token_deposit_account: owner_token_1,
+            base_vault,
+            quote_vault,
+            side: Side::Bid,
+            price_lots,
+            max_base_lots: 1,
+            max_quote_lots_including_fees: 10000,
+
+            client_order_id: 0,
+            expiry_timestamp: 0,
+            order_type: PlaceOrderType::Limit,
+            self_trade_behavior: SelfTradeBehavior::default(),
+            remainings: vec![],
+        },
+    )
+    .await
+    .unwrap();
+
+    // Can't close yet
+    let result = send_tx(
+        solana,
+        CloseMarketInstruction {
+            close_market_admin,
+            market,
+            sol_destination: owner.pubkey(),
+        },
+    )
+    .await;
+    assert!(result.is_err());
+
+    send_tx(
+        solana,
+        SetMarketExpiredInstruction {
+            close_market_admin,
+            market,
+        },
+    )
+    .await
+    .unwrap();
+
+    {
+        let market = solana.get_account::<Market>(market).await;
+        assert_eq!(market.time_expiry, -1);
+    }
+
+    // Can't post orders anymore
+
+    let result = send_tx(
+        solana,
+        PlaceOrderInstruction {
+            open_orders_account: account_0,
+            open_orders_admin: None,
+            market,
+            owner,
+            token_deposit_account: owner_token_1,
+            base_vault,
+            quote_vault,
+            side: Side::Bid,
+            price_lots,
+            max_base_lots: 1,
+            max_quote_lots_including_fees: 10000,
+
+            client_order_id: 0,
+            expiry_timestamp: 0,
+            order_type: PlaceOrderType::Limit,
+            self_trade_behavior: SelfTradeBehavior::default(),
+            remainings: vec![],
+        },
+    )
+    .await;
+    assert!(result.is_err());
+
+    // Consume events
+    send_tx(
+        solana,
+        ConsumeEventsInstruction {
+            consume_events_admin: None,
+            market,
+            open_orders_accounts: vec![account_0, account_1],
+        },
+    )
+    .await
+    .unwrap();
+
+    // Can't close, have to prune orders first
+    let result = send_tx(
+        solana,
+        CloseMarketInstruction {
+            close_market_admin,
+            market,
+            sol_destination: owner.pubkey(),
+        },
+    )
+    .await;
+    assert!(result.is_err());
+
+    //Prune order
+    send_tx(
+        solana,
+        PruneOrdersInstruction {
+            close_market_admin,
+            market,
+            open_orders_account: account_0,
+        },
+    )
+    .await
+    .unwrap();
+
+    // Boom
+    send_tx(
+        solana,
+        CloseMarketInstruction {
+            close_market_admin,
+            market,
+            sol_destination: owner.pubkey(),
+        },
+    )
+    .await
+    .unwrap();
+
+    Ok(())
+}
