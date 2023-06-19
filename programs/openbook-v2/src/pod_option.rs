5b5242eaacf6bc4998faec0e2bb6eeebf64766e5
skrrb <skrrb@pm.me>
2023-06-19 12:09:45 +0300

    derive anchor serialize for podoption

--- programs/openbook-v2/src/pod_option.rs
+++ programs/openbook-v2/src/pod_option.rs
@@ -1,6 +1,4 @@
-#[cfg(test)]
-use anchor_lang::prelude::Pubkey;
-use borsh::{BorshDeserialize, BorshSerialize};
+use anchor_lang::prelude::*;
 use bytemuck::{Pod, Zeroable};
 use std::convert::From;
 
@@ -9,7 +7,7 @@ use std::convert::From;
 /// To ensure that there are no illegal bit patterns or padding bytes,
 /// `PodOption` is laid out as a single byte which is 0 in the case of `None`
 /// or non-zero in the case of `Some`, and then the value, if any.
-#[derive(Copy, Clone, Debug)]
+#[derive(AnchorSerialize, AnchorDeserialize, Default, Copy, Clone, Debug)]
 #[repr(C)]
 pub struct PodOption<T: Pod> {
     flag: u64,
@@ -40,44 +38,6 @@ impl<T: Pod> From<Option<T>> for PodOption<T> {
                 flag: 0,
                 value: T::zeroed(),
             },
-        }
-    }
-}
-
-impl<T: Pod> BorshDeserialize for PodOption<T>
-where
-    T: BorshSerialize + BorshDeserialize + Default,
-{
-    fn deserialize(buf: &mut &[u8]) -> std::io::Result<Self> {
-        let flag = u64::deserialize(buf)?;
-        let value = if flag != 0 {
-            T::deserialize(buf)?
-        } else {
-            T::default()
-        };
-        Ok(PodOption { flag, value })
-    }
-}
-
-impl<T: Pod> BorshSerialize for PodOption<T>
-where
-    T: BorshSerialize + BorshDeserialize,
-{
-    fn serialize<W: std::io::Write>(&self, writer: &mut W) -> std::io::Result<()> {
-        self.flag.serialize(writer)?;
-        self.value.serialize(writer)?;
-        Ok(())
-    }
-}
-
-impl<T: Pod> Default for PodOption<T>
-where
-    T: BorshSerialize + BorshDeserialize + Default,
-{
-    fn default() -> Self {
-        PodOption {
-            flag: 0,
-            value: T::default(),
         }
     }
 }
