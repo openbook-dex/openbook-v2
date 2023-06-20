8be941bd7fc4ef9f7e6576325a2a0ad261145ad3
Binye Barwe <binyebarwe@gmail.com>
2023-06-20 13:49:56 +0200

    add different test fees

--- programs/openbook-v2/tests/cases/test_fees.rs
+++ programs/openbook-v2/tests/cases/test_fees.rs
@@ -858,6 +858,7 @@ async fn test_fees_half() -> Result<(), TransportError> {
 
     // Set the initial oracle price
     set_stub_oracle_price(solana, &tokens[1], collect_fee_admin, 1000.0).await;
+    let initial_quote_amount = 10000;
 
     send_tx(
         solana,
@@ -872,7 +873,7 @@ async fn test_fees_half() -> Result<(), TransportError> {
             side: Side::Bid,
             price_lots,
             max_base_lots: 1,
-            max_quote_lots_including_fees: 10000,
+            max_quote_lots_including_fees: initial_quote_amount,
             client_order_id: 0,
             expiry_timestamp: 0,
             order_type: PlaceOrderType::Limit,
@@ -883,7 +884,6 @@ async fn test_fees_half() -> Result<(), TransportError> {
     .await
     .unwrap();
 
-    let balance_base = solana.token_account_balance(owner_token_0).await;
     send_tx(
         solana,
         PlaceOrderInstruction {
@@ -897,7 +897,7 @@ async fn test_fees_half() -> Result<(), TransportError> {
             side: Side::Ask,
             price_lots,
             max_base_lots: 1,
-            max_quote_lots_including_fees: 10000,
+            max_quote_lots_including_fees: initial_quote_amount,
             client_order_id: 0,
             expiry_timestamp: 0,
             order_type: PlaceOrderType::Limit,
@@ -922,12 +922,18 @@ async fn test_fees_half() -> Result<(), TransportError> {
     {
         let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
         let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
+        let market = solana.get_account::<Market>(market).await;
 
         assert_eq!(open_orders_account_0.position.quote_free_native, 371);
         assert_eq!(open_orders_account_1.position.quote_free_native, 99259);
-
-        let market = solana.get_account::<Market>(market).await;
         assert_eq!(market.referrer_rebates_accrued, 370);
+
+        assert_eq!(
+            (market.referrer_rebates_accrued
+                + open_orders_account_1.position.quote_free_native
+                + open_orders_account_0.position.quote_free_native) as i64,
+            initial_quote_amount * 10 // as it's in native quote
+        );
     }
 
     let admin_token_1 = solana
@@ -949,6 +955,7 @@ async fn test_fees_half() -> Result<(), TransportError> {
     )
     .await
     .unwrap();
+
     send_tx(
         solana,
         SettleFundsInstruction {
@@ -966,11 +973,6 @@ async fn test_fees_half() -> Result<(), TransportError> {
     .unwrap();
 
     {
-        let market = solana.get_account::<Market>(market).await;
-        // assert_eq!(market.quote_fees_accrued, 10);
-        // assert_eq!(market.fees_accrued, 10);
-        // assert_eq!(market.fees_to_referrers, 0);
-
         let balance_quote = solana.token_account_balance(quote_vault).await;
         assert_eq!(balance_quote, 370);
     }
@@ -987,17 +989,171 @@ async fn test_fees_half() -> Result<(), TransportError> {
     .await
     .unwrap();
 
-    let balance_quote = solana.token_account_balance(quote_vault).await;
+    {
+        let balance_quote = solana.token_account_balance(quote_vault).await;
+        assert_eq!(balance_quote, 0);
+    }
+
+    // Different fees
+
+    let TestInitialize {
+        context,
+        collect_fee_admin,
+        owner,
+        mints,
+        owner_token_0,
+        owner_token_1,
+        market,
+        base_vault,
+        quote_vault,
+        price_lots,
+        tokens,
+        account_0,
+        account_1,
+        ..
+    } = TestContext::new_with_market(TestNewMarketInitialize {
+        maker_fee: -0.0032,
+        taker_fee: 0.0064,
+        ..TestNewMarketInitialize::default()
+    })
+    .await?;
+    let solana = &context.solana.clone();
+
+    // Set the initial oracle price
+    set_stub_oracle_price(solana, &tokens[1], collect_fee_admin, 1000.0).await;
+    let initial_quote_amount = 10000;
+
+    send_tx(
+        solana,
+        PlaceOrderInstruction {
+            open_orders_account: account_0,
+            open_orders_admin: None,
+            market,
+            owner,
+            token_deposit_account: owner_token_1,
+            base_vault,
+            quote_vault,
+            side: Side::Bid,
+            price_lots,
+            max_base_lots: 1,
+            max_quote_lots_including_fees: initial_quote_amount,
+            client_order_id: 0,
+            expiry_timestamp: 0,
+            order_type: PlaceOrderType::Limit,
+            self_trade_behavior: SelfTradeBehavior::default(),
+            remainings: vec![],
+        },
+    )
+    .await
+    .unwrap();
+
+    send_tx(
+        solana,
+        PlaceOrderInstruction {
+            open_orders_account: account_1,
+            open_orders_admin: None,
+            market,
+            owner,
+            token_deposit_account: owner_token_0,
+            base_vault,
+            quote_vault,
+            side: Side::Ask,
+            price_lots,
+            max_base_lots: 1,
+            max_quote_lots_including_fees: initial_quote_amount,
+            client_order_id: 0,
+            expiry_timestamp: 0,
+            order_type: PlaceOrderType::Limit,
+            self_trade_behavior: SelfTradeBehavior::default(),
+            remainings: vec![],
+        },
+    )
+    .await
+    .unwrap();
+
+    send_tx(
+        solana,
+        ConsumeEventsInstruction {
+            consume_events_admin: None,
+            market,
+            open_orders_accounts: vec![account_0, account_1],
+        },
+    )
+    .await
+    .unwrap();
 
     {
-        // let market = solana.get_account::<Market>(market).await;
-        // assert_eq!(market.quote_fees_accrued, 1000);
-        // assert_eq!(market.fees_accrued, 10);
-        // assert_eq!(market.fees_to_referrers, 0);
-        // assert_eq!(
-        //     balance_quote - 100,
-        //     solana.token_account_balance(owner_token_1).await
-        // );
+        let open_orders_account_0 = solana.get_account::<OpenOrdersAccount>(account_0).await;
+        let open_orders_account_1 = solana.get_account::<OpenOrdersAccount>(account_1).await;
+        let market = solana.get_account::<Market>(market).await;
+
+        assert_eq!(open_orders_account_0.position.quote_free_native, 320);
+        assert_eq!(open_orders_account_1.position.quote_free_native, 99360);
+        assert_eq!(market.referrer_rebates_accrued, 320);
+
+        assert_eq!(
+            (market.referrer_rebates_accrued
+                + open_orders_account_1.position.quote_free_native
+                + open_orders_account_0.position.quote_free_native) as i64,
+            initial_quote_amount * 10 // as it's in native quote
+        );
+    }
+
+    let admin_token_1 = solana
+        .create_associated_token_account(&collect_fee_admin.pubkey(), mints[1].pubkey)
+        .await;
+
+    send_tx(
+        solana,
+        SettleFundsInstruction {
+            owner,
+            market,
+            open_orders_account: account_1,
+            base_vault,
+            quote_vault,
+            token_base_account: owner_token_0,
+            token_quote_account: owner_token_1,
+            referrer: None,
+        },
+    )
+    .await
+    .unwrap();
+
+    send_tx(
+        solana,
+        SettleFundsInstruction {
+            owner,
+            market,
+            open_orders_account: account_0,
+            base_vault,
+            quote_vault,
+            token_base_account: owner_token_0,
+            token_quote_account: owner_token_1,
+            referrer: None,
+        },
+    )
+    .await
+    .unwrap();
+
+    {
+        let balance_quote = solana.token_account_balance(quote_vault).await;
+        assert_eq!(balance_quote, 320);
+    }
+
+    send_tx(
+        solana,
+        SweepFeesInstruction {
+            collect_fee_admin,
+            market,
+            quote_vault,
+            token_receiver_account: admin_token_1,
+        },
+    )
+    .await
+    .unwrap();
+
+    {
+        let balance_quote = solana.token_account_balance(quote_vault).await;
         assert_eq!(balance_quote, 0);
     }
 
