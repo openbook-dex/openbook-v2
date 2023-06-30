61eef830aaf2f5e3ea4e31466b62523bdd18978f
skrrb <skrrb@pm.me>
2023-06-30 11:20:32 +0300

    fmt

--- programs/openbook-v2/src/instructions/settle_funds.rs
+++ programs/openbook-v2/src/instructions/settle_funds.rs
@@ -5,24 +5,25 @@ use crate::accounts_ix::*;
 use crate::state::*;
 
 pub fn settle_funds<'info>(ctx: Context<'_, '_, '_, 'info, SettleFunds<'info>>) -> Result<()> {
-    let mut acc = ctx.accounts.open_orders_account.load_mut()?;
+    let pa = &mut ctx.accounts.open_orders_account.load_mut()?.position;
     let mut market = ctx.accounts.market.load_mut()?;
 
     if ctx.remaining_accounts.is_empty() {
-        market.quote_fees_accrued += acc.position.referrer_rebates_accrued;
+        market.quote_fees_accrued += pa.referrer_rebates_accrued;
     } else {
-        market.fees_to_referrers += acc.position.referrer_rebates_accrued;
+        market.fees_to_referrers += pa.referrer_rebates_accrued;
     }
-    market.referrer_rebates_accrued -= acc.position.referrer_rebates_accrued;
-    market.base_deposit_total -= acc.position.base_free_native;
-    market.quote_deposit_total -= acc.position.quote_free_native;
+
+    market.referrer_rebates_accrued -= pa.referrer_rebates_accrued;
+    market.base_deposit_total -= pa.base_free_native;
+    market.quote_deposit_total -= pa.quote_free_native;
 
     let seeds = market_seeds!(market);
     let signer = &[&seeds[..]];
 
     drop(market);
 
-    if !ctx.remaining_accounts.is_empty() && acc.position.referrer_rebates_accrued > 0 {
+    if !ctx.remaining_accounts.is_empty() && pa.referrer_rebates_accrued > 0 {
         let referrer = ctx.remaining_accounts[0].to_account_info();
         let cpi_context = CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
@@ -32,13 +33,10 @@ pub fn settle_funds<'info>(ctx: Context<'_, '_, '_, 'info, SettleFunds<'info>>) 
                 authority: ctx.accounts.market.to_account_info(),
             },
         );
-        token::transfer(
-            cpi_context.with_signer(signer),
-            acc.position.referrer_rebates_accrued,
-        )?;
+        token::transfer(cpi_context.with_signer(signer), pa.referrer_rebates_accrued)?;
     }
 
-    if acc.position.base_free_native > 0 {
+    if pa.base_free_native > 0 {
         let cpi_context = CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
             Transfer {
@@ -47,13 +45,10 @@ pub fn settle_funds<'info>(ctx: Context<'_, '_, '_, 'info, SettleFunds<'info>>) 
                 authority: ctx.accounts.market.to_account_info(),
             },
         );
-        token::transfer(
-            cpi_context.with_signer(signer),
-            acc.position.base_free_native,
-        )?;
+        token::transfer(cpi_context.with_signer(signer), pa.base_free_native)?;
     }
 
-    if acc.position.quote_free_native > 0 {
+    if pa.quote_free_native > 0 {
         let cpi_context = CpiContext::new(
             ctx.accounts.token_program.to_account_info(),
             Transfer {
@@ -62,16 +57,13 @@ pub fn settle_funds<'info>(ctx: Context<'_, '_, '_, 'info, SettleFunds<'info>>) 
                 authority: ctx.accounts.market.to_account_info(),
             },
         );
-        token::transfer(
-            cpi_context.with_signer(signer),
-            acc.position.quote_free_native,
-        )?;
+        token::transfer(cpi_context.with_signer(signer), pa.quote_free_native)?;
     }
 
     // Set to 0 after transfer
-    acc.position.base_free_native = 0;
-    acc.position.quote_free_native = 0;
-    acc.position.referrer_rebates_accrued = 0;
+    pa.base_free_native = 0;
+    pa.quote_free_native = 0;
+    pa.referrer_rebates_accrued = 0;
 
     Ok(())
 }
