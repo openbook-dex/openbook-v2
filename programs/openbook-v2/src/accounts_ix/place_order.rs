060d7f86bc79a847817af0e27e582486184de69e
skrrb <skrrb@pm.me>
2023-07-24 10:43:09 +0200

    validate owner or delegate in accounts struct

--- programs/openbook-v2/src/accounts_ix/place_order.rs
+++ programs/openbook-v2/src/accounts_ix/place_order.rs
@@ -1,16 +1,17 @@
+use crate::error::OpenBookError;
 use crate::state::*;
 use anchor_lang::prelude::*;
 use anchor_spl::token::{Token, TokenAccount};
 
 #[derive(Accounts)]
 pub struct PlaceOrder<'info> {
+    pub signer: Signer<'info>,
     #[account(
         mut,
         has_one = market,
-        // also is_owner_or_delegate check inside ix
+        constraint = open_orders_account.load()?.is_owner_or_delegate(signer.key()) @ OpenBookError::NoOwnerOrDelegate
     )]
     pub open_orders_account: AccountLoader<'info, OpenOrdersAccount>,
-    pub owner_or_delegate: Signer<'info>,
     pub open_orders_admin: Option<Signer<'info>>,
 
     #[account(
--- programs/openbook-v2/src/instructions/place_order.rs
+++ programs/openbook-v2/src/instructions/place_order.rs
@@ -17,10 +17,6 @@ pub fn place_order(ctx: Context<PlaceOrder>, order: Order, limit: u8) -> Result<
     );
 
     let mut open_orders_account = ctx.accounts.open_orders_account.load_mut()?;
-    require!(
-        open_orders_account.is_owner_or_delegate(ctx.accounts.owner_or_delegate.key()),
-        OpenBookError::NoOwnerOrDelegate
-    );
     let open_orders_account_pk = ctx.accounts.open_orders_account.key();
 
     let mut market = ctx.accounts.market.load_mut()?;
@@ -123,7 +119,7 @@ pub fn place_order(ctx: Context<PlaceOrder>, order: Order, limit: u8) -> Result<
             Transfer {
                 from: ctx.accounts.token_deposit_account.to_account_info(),
                 to: ctx.accounts.market_vault.to_account_info(),
-                authority: ctx.accounts.owner_or_delegate.to_account_info(),
+                authority: ctx.accounts.signer.to_account_info(),
             },
         );
         token::transfer(cpi_context, deposit_amount)?;
