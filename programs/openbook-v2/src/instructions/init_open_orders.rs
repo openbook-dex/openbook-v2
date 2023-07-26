380774652f0f9c27f47ae483d34450e987e71cd0
skrrb <skrrb@pm.me>
2023-07-26 08:38:05 +0200

    use trait

--- programs/openbook-v2/src/instructions/init_open_orders.rs
+++ programs/openbook-v2/src/instructions/init_open_orders.rs
@@ -1,23 +1,16 @@
 use crate::accounts_ix::InitOpenOrders;
-use crate::pubkey_option::NonZeroPubkeyOption;
+use crate::pubkey_option::NonZeroKey;
 use crate::state::*;
 use anchor_lang::prelude::*;
 
 pub fn init_open_orders(ctx: Context<InitOpenOrders>, account_num: u32) -> Result<()> {
     let mut account = ctx.accounts.open_orders_account.load_init()?;
 
-    let delegate_account: NonZeroPubkeyOption = ctx
-        .accounts
-        .delegate_account
-        .as_ref()
-        .map(|account| account.key())
-        .into();
-
     account.account_num = account_num;
     account.market = ctx.accounts.market.key();
     account.bump = *ctx.bumps.get("open_orders_account").unwrap();
     account.owner = ctx.accounts.owner.key();
-    account.delegate = delegate_account;
+    account.delegate = ctx.accounts.delegate_account.non_zero_key();
     account.open_orders = [OpenOrder::default(); MAX_OPEN_ORDERS];
 
     Ok(())
