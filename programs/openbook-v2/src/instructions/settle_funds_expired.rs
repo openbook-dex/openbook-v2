28e49c30b23265fc1ab5dd14a8d27c035b02b8e9
skrrb <skrrb@pm.me>
2023-08-01 10:07:35 +0200

    fix borrow check

--- programs/openbook-v2/src/instructions/settle_funds_expired.rs
+++ programs/openbook-v2/src/instructions/settle_funds_expired.rs
@@ -5,11 +5,13 @@ use anchor_lang::prelude::*;
 pub fn settle_funds_expired<'info>(
     ctx: Context<'_, '_, '_, 'info, SettleFundsExpired<'info>>,
 ) -> Result<()> {
-    let market = ctx.accounts.market.load()?;
-    require!(
-        market.is_expired(Clock::get()?.unix_timestamp),
-        OpenBookError::MarketHasNotExpired
-    );
+    {
+        let market = ctx.accounts.market.load()?;
+        require!(
+            market.is_expired(Clock::get()?.unix_timestamp),
+            OpenBookError::MarketHasNotExpired
+        );
+    }
 
     crate::instructions::settle_funds(Context::new(
         ctx.program_id,
