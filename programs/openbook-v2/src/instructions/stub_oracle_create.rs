c0f8682b6c6000a89c26c2f6bb66d3cc2bc3d4cd
skrrb <skrrb@pm.me>
2023-08-25 10:33:33 +0200

    stub oracle update slot

--- programs/openbook-v2/src/instructions/stub_oracle_create.rs
+++ programs/openbook-v2/src/instructions/stub_oracle_create.rs
@@ -3,11 +3,14 @@ use anchor_lang::prelude::*;
 use crate::accounts_ix::*;
 
 pub fn stub_oracle_create(ctx: Context<StubOracleCreate>, price: f64) -> Result<()> {
+    let clock = Clock::get()?;
     let mut oracle = ctx.accounts.oracle.load_init()?;
+
     oracle.owner = ctx.accounts.owner.key();
     oracle.mint = ctx.accounts.mint.key();
     oracle.price = price;
-    oracle.last_update_ts = Clock::get()?.unix_timestamp;
+    oracle.last_update_ts = clock.unix_timestamp;
+    oracle.last_update_slot = clock.slot;
 
     Ok(())
 }
--- programs/openbook-v2/src/instructions/stub_oracle_set.rs
+++ programs/openbook-v2/src/instructions/stub_oracle_set.rs
@@ -2,9 +2,12 @@ use crate::accounts_ix::*;
 use anchor_lang::prelude::*;
 
 pub fn stub_oracle_set(ctx: Context<StubOracleSet>, price: f64) -> Result<()> {
+    let clock = Clock::get()?;
     let mut oracle = ctx.accounts.oracle.load_mut()?;
+
     oracle.price = price;
-    oracle.last_update_ts = Clock::get()?.unix_timestamp;
+    oracle.last_update_ts = clock.unix_timestamp;
+    oracle.last_update_slot = clock.slot;
 
     Ok(())
 }
