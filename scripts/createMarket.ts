6f4109309f513177165537120f794d0ec5bff21c
metaproph3t <metaproph3t@protonmail.com>
2023-10-24 00:00:00 +0000

    Add script scaffold

---
+++ scripts/createMarket.ts
@@ -0,0 +1,7 @@
+import {
+  OpenBookV2Client
+} from "@openbook-dex/openbook-v2";
+import * as anchor from "@coral-xyz/anchor";
+
+const provider = anchor.AnchorProvider.env();
+anchor.setProvider(provider);
