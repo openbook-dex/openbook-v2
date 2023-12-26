37077bc675e2e03cf4259e9ade472dbac64062ec
skrrb <skrrb@pm.me>
2023-12-26 21:40:01 +0100

    test

--- programs/openbook-v2/tests/cases/test_multiple_orders.rs
+++ programs/openbook-v2/tests/cases/test_multiple_orders.rs
@@ -2,9 +2,6 @@ use super::*;
 
 #[tokio::test]
 async fn insufficient_funds() -> Result<(), TransportError> {
-    let base_lot_size = 100;
-    let quote_lot_size = 10;
-
     let TestInitialize {
         context,
         owner,
@@ -16,14 +13,11 @@ async fn insufficient_funds() -> Result<(), TransportError> {
         market_base_vault,
         market_quote_vault,
         ..
-    } = TestContext::new_with_market(TestNewMarketInitialize {
-        base_lot_size,
-        quote_lot_size,
-        ..TestNewMarketInitialize::default()
-    })
-    .await?;
+    } = TestContext::new_with_market(TestNewMarketInitialize::default()).await?;
 
     let solana = &context.solana.clone();
+
+    let max_quote_lots_including_fees = 104;
 
     // there's an ask on the book
     send_tx(
@@ -37,8 +31,8 @@ async fn insufficient_funds() -> Result<(), TransportError> {
             market_vault: market_base_vault,
             side: Side::Ask,
             price_lots: 1,
-            max_base_lots: 10,
-            max_quote_lots_including_fees: i64::MAX / 1_000_000,
+            max_base_lots: i64::MAX / 1_000,
+            max_quote_lots_including_fees,
             client_order_id: 0,
             expiry_timestamp: 0,
             order_type: PlaceOrderType::Limit,
@@ -72,47 +66,27 @@ async fn insufficient_funds() -> Result<(), TransportError> {
 
     // note that a priori, we only have enough lamports to place 2.5 Ask. But as the bid will be
     // filled & the taker executed immediately, we will have 10 extra base lots available
-    let place_orders = (0..5)
-        .map(|i| {
-            if i == 1 {
-                openbook_v2::PlaceOrderArgs {
-                    side: Side::Bid,
-                    price_lots: 1,
-                    max_base_lots: 10,
-                    max_quote_lots_including_fees: i64::MAX / 1_000_000,
-                    client_order_id: 0,
-                    order_type: PlaceOrderType::Limit,
-                    expiry_timestamp: 0,
-                    self_trade_behavior: SelfTradeBehavior::default(),
-                    limit: 10,
-                }
-            } else {
-                openbook_v2::PlaceOrderArgs {
-                    side: Side::Ask,
-                    price_lots: 1,
-                    max_base_lots: 10,
-                    max_quote_lots_including_fees: i64::MAX / 1_000_000,
-                    client_order_id: 0,
-                    order_type: PlaceOrderType::Limit,
-                    expiry_timestamp: 0,
-                    self_trade_behavior: SelfTradeBehavior::default(),
-                    limit: 10,
-                }
-            }
-        })
-        .collect::<Vec<_>>();
+    let order = openbook_v2::PlaceMultipleOrdersArgs {
+        price_lots: 1,
+        max_quote_lots_including_fees,
+        expiry_timestamp: 0,
+    };
+
+    let bids = vec![order];
+    let asks = vec![order; 4];
 
     send_tx(
         solana,
-        CancelAndPlaceOrdersInstruction {
+        CancelAllAndPlaceOrdersInstruction {
             open_orders_account: account_1,
             open_orders_admin: None,
             market,
             signer: owner,
+            orders_type: PlaceOrderType::Limit,
             user_base_account: owner_token_0,
             user_quote_account: owner_token_1,
-            cancel_client_orders_ids: vec![],
-            place_orders,
+            bids,
+            asks,
         },
     )
     .await
