af8a88a9dec58a0f6d4cd29ba826e2343bbe5709
Lou-Kamades <lou_kamades@proton.me>
2023-12-30 14:56:48 -0600

    add consume events to rust client

--- lib/client/src/client.rs
+++ lib/client/src/client.rs
@@ -514,7 +514,7 @@ impl OpenBookClient {
                 )
             },
             data: anchor_lang::InstructionData::data(&openbook_v2::instruction::CancelOrder {
-                order_id
+                order_id,
             }),
         };
         self.send_and_confirm_owner_tx(vec![ix]).await
@@ -544,7 +544,7 @@ impl OpenBookClient {
             },
             data: anchor_lang::InstructionData::data(&openbook_v2::instruction::CancelAllOrders {
                 side_option,
-                limit
+                limit,
             }),
         };
         self.send_and_confirm_owner_tx(vec![ix]).await
@@ -619,6 +619,32 @@ impl OpenBookClient {
                 )
             },
             data: anchor_lang::InstructionData::data(&openbook_v2::instruction::SettleFunds {}),
+        };
+        self.send_and_confirm_owner_tx(vec![ix]).await
+    }
+
+    #[allow(clippy::too_many_arguments)]
+    pub async fn consume_events(
+        &self,
+        market: Market,
+        market_address: Pubkey,
+        limit: usize,
+    ) -> anyhow::Result<Signature> {
+        let ix = Instruction {
+            program_id: openbook_v2::id(),
+            accounts: {
+                anchor_lang::ToAccountMetas::to_account_metas(
+                    &openbook_v2::accounts::ConsumeEvents {
+                        consume_events_admin: market.consume_events_admin.into(),
+                        market: market_address,
+                        event_heap: market.event_heap,
+                    },
+                    None,
+                )
+            },
+            data: anchor_lang::InstructionData::data(&openbook_v2::instruction::ConsumeEvents {
+                limit,
+            }),
         };
         self.send_and_confirm_owner_tx(vec![ix]).await
     }
