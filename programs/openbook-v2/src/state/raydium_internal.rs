49fff9fa9d213dfa0af59c7911701e89df2297a3
Serge Farny <serge.farny@gmail.com>
2024-04-12 10:29:01 +0200

    program: upgrade to solana 1.17

---
+++ programs/openbook-v2/src/state/raydium_internal.rs
@@ -0,0 +1,129 @@
+use anchor_lang::{account, declare_id, zero_copy};
+use solana_program::pubkey::Pubkey;
+
+#[cfg(feature = "devnet")]
+declare_id!("devi51mZmdwUJGU9hjN27vEz64Gps7uUefqxg27EAtH");
+#[cfg(not(feature = "devnet"))]
+declare_id!("CAMMCzo5YL8w4VFF8KVHrK22GGUsp5VTaW7grrKgrWqK");
+
+pub const REWARD_NUM: usize = 3;
+
+/// The pool state
+///
+/// PDA of `[POOL_SEED, config, token_mint_0, token_mint_1]`
+///
+#[account(zero_copy(unsafe))]
+#[repr(packed)]
+#[derive(Default, Debug)]
+pub struct PoolState {
+    /// Bump to identify PDA
+    pub bump: [u8; 1],
+    // Which config the pool belongs
+    pub amm_config: Pubkey,
+    // Pool creator
+    pub owner: Pubkey,
+
+    /// Token pair of the pool, where token_mint_0 address < token_mint_1 address
+    pub token_mint_0: Pubkey,
+    pub token_mint_1: Pubkey,
+
+    /// Token pair vault
+    pub token_vault_0: Pubkey,
+    pub token_vault_1: Pubkey,
+
+    /// observation account key
+    pub observation_key: Pubkey,
+
+    /// mint0 and mint1 decimals
+    pub mint_decimals_0: u8,
+    pub mint_decimals_1: u8,
+
+    /// The minimum number of ticks between initialized ticks
+    pub tick_spacing: u16,
+    /// The currently in range liquidity available to the pool.
+    pub liquidity: u128,
+    /// The current price of the pool as a sqrt(token_1/token_0) Q64.64 value
+    pub sqrt_price_x64: u128,
+    /// The current tick of the pool, i.e. according to the last tick transition that was run.
+    pub tick_current: i32,
+
+    /// the most-recently updated index of the observations array
+    pub observation_index: u16,
+    pub observation_update_duration: u16,
+
+    /// The fee growth as a Q64.64 number, i.e. fees of token_0 and token_1 collected per
+    /// unit of liquidity for the entire life of the pool.
+    pub fee_growth_global_0_x64: u128,
+    pub fee_growth_global_1_x64: u128,
+
+    /// The amounts of token_0 and token_1 that are owed to the protocol.
+    pub protocol_fees_token_0: u64,
+    pub protocol_fees_token_1: u64,
+
+    /// The amounts in and out of swap token_0 and token_1
+    pub swap_in_amount_token_0: u128,
+    pub swap_out_amount_token_1: u128,
+    pub swap_in_amount_token_1: u128,
+    pub swap_out_amount_token_0: u128,
+
+    /// Bitwise representation of the state of the pool
+    /// bit0, 1: disable open position and increase liquidity, 0: normal
+    /// bit1, 1: disable decrease liquidity, 0: normal
+    /// bit2, 1: disable collect fee, 0: normal
+    /// bit3, 1: disable collect reward, 0: normal
+    /// bit4, 1: disable swap, 0: normal
+    pub status: u8,
+    /// Leave blank for future use
+    pub padding: [u8; 7],
+
+    pub reward_infos: [RewardInfo; REWARD_NUM],
+
+    /// Packed initialized tick array state
+    pub tick_array_bitmap: [u64; 16],
+
+    /// except protocol_fee and fund_fee
+    pub total_fees_token_0: u64,
+    /// except protocol_fee and fund_fee
+    pub total_fees_claimed_token_0: u64,
+    pub total_fees_token_1: u64,
+    pub total_fees_claimed_token_1: u64,
+
+    pub fund_fees_token_0: u64,
+    pub fund_fees_token_1: u64,
+
+    // The timestamp allowed for swap in the pool.
+    pub open_time: u64,
+
+    // Unused bytes for future upgrades.
+    pub padding1: [u64; 25],
+    pub padding2: [u64; 32],
+}
+
+#[zero_copy(unsafe)]
+#[repr(packed)]
+#[derive(Default, Debug, PartialEq, Eq)]
+pub struct RewardInfo {
+    /// Reward state
+    pub reward_state: u8,
+    /// Reward open time
+    pub open_time: u64,
+    /// Reward end time
+    pub end_time: u64,
+    /// Reward last update time
+    pub last_update_time: u64,
+    /// Q64.64 number indicates how many tokens per second are earned per unit of liquidity.
+    pub emissions_per_second_x64: u128,
+    /// The total amount of reward emissioned
+    pub reward_total_emissioned: u64,
+    /// The total amount of claimed reward
+    pub reward_claimed: u64,
+    /// Reward token mint.
+    pub token_mint: Pubkey,
+    /// Reward vault token account.
+    pub token_vault: Pubkey,
+    /// The owner that has permission to set reward param
+    pub authority: Pubkey,
+    /// Q64.64 number that tracks the total tokens earned per unit of liquidity since the reward
+    /// emissions were turned on.
+    pub reward_growth_global_x64: u128,
+}
