b954445f532ee1b54fb404139ba3617ec12eb636
Serge Farny <serge.farny@gmail.com>
2024-06-04 01:20:00 +0200

    Program: move fill or kill data at the end of enum for backward compatibility (#269)

--- programs/openbook-v2/src/error.rs
+++ programs/openbook-v2/src/error.rs
@@ -89,13 +89,12 @@ pub enum OpenBookError {
     InvalidPostAmount,
     #[msg("Oracle peg orders are not enabled for this market")]
     DisabledOraclePeg,
-    #[msg("Fill-Or-Kill order would generate a partial execution")]
-    WouldExecutePartially,
-
     #[msg("Cannot close a non-empty market")]
     NonEmptyMarket,
     #[msg("Cannot close a non-empty open orders account")]
     NonEmptyOpenOrdersPosition,
+    #[msg("Fill-Or-Kill order would generate a partial execution")]
+    WouldExecutePartially,
 }
 
 impl From<OpenBookError> for ProgramError {
